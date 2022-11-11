0d33818c958bda11e123078a84a9532ed9574275
Billy Salinas <wsalinas315@gmail.com>
2022-11-11 16:26:47 -0600

    outlined client.js

---
+++ server/public/scripts/client.js
@@ -0,0 +1,23 @@
+$(document).ready(onReady);
+
+// on document load
+function onReady(){
+
+    // call get
+}
+
+// GET TASKS
+
+
+// RENDER TO DOM
+
+
+// POST TASKS
+
+
+// PUT TASKS
+
+
+// DELETE TASKS
+
+
