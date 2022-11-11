c1f4a8561859d2dba1769cc89e203488902833ac
Billy Salinas <wsalinas315@gmail.com>
2022-11-11 16:46:39 -0600

    setup databse.sql file with initial SQL commands

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tasks-list (
+    id SERIAL,
+    taskName varchar(64),
+    taskDetails varchar(128),
+    complete BOOLEAN NOT NULL,
+    compDate date 
+);
+
+INSERT INTO "tasks-list"
+    ("taskName","taskDetails","complete","compDate")
+VALUES
+    ('New Fridge','Purchase and install new fridge, haul away old one.', 'FALSE', NULL),
+    ('Rake Leaves','Rake & bag leaves. Take them to compost.', 'FALSE', NULL),
+    ('Feed Cat','Wet food for the kitty!', 'FALSE', NULL),
+    ('New Tires','Purchase new tires and setup installment appointment.', 'FALSE', NULL),
+    ('Finish Weekend Project','ALL THE CODE, ALL THE COMMITS! GIT GOOD NEWB!', 'FALSE', NULL);
