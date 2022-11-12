4d6db7b6700d58394881da17fe6eba943aafb2e4
Billy Salinas <wsalinas315@gmail.com>
2022-11-12 17:46:55 -0600

    wrote taskRouter.get for tasks.router.js

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -3,7 +3,16 @@ const tasksRouter = express.Router();
 const pool = require('../modules/pool');
 
 // tasksRouter GET
-
+tasksRouter.get('/', (req, res) => {
+    let taskQuery = `SELECT * FROM "tasks-list" ORDER BY "complete" DESC;`;
+    pool.query(taskQuery).then((result) => {
+        console.log('result.rows:', result.rows); // comment this one if terminal is too spammy
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(`Error making query: ${taskQuery}, error is:`, error);
+        sendStatus(500);
+    });
+});
 
 // tasksRouter POST
 
