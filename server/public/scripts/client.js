b17ceff0cf98f594ca299b6c63c31233d9d39bbc
Billy Salinas <wsalinas315@gmail.com>
2022-11-12 16:38:44 -0600

    getTasks function written

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,11 +3,28 @@ $(document).ready(onReady);
 // on document load
 function onReady(){
 
+    // setup click listeners
+
     // call get
 }
 
-// GET TASKS
+// CLICK listeners
 
+
+// GET function to retrieve tasks
+function getTasks(){
+    console.log('in getTasks client function');
+	// ajax call to server to get koalas
+	$.ajax({
+		method: 'GET',
+		url: '/tasks',
+	}).then(function (response) {
+		console.log(response);
+		renderTasks(response);
+	}).catch(function (error) {
+		console.log('Error in GET:', error);
+	});
+}
 
 // RENDER TO DOM
 
